c724c81e6f3b06adeea71e2203de3cc54432edc5
kjgrover <kaje14@bellsouth.net>
2018-08-02 15:28:33 -0600

    changed port to 8080

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require("path");
 // =============================================================
 var app = express();
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 app.use(express.static('public'));
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
