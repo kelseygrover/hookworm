8446714b576cf21dba8cfbad8513468443160357
kjgrover <kaje14@bellsouth.net>
2018-08-04 23:06:47 -0600

    fixed photots

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 app.use(express.static('public'));
+app.use(express.static('/Images'));
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
