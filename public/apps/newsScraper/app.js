d551febd3a623c9fd19a9fd9fa97817c04d5477a
kjgrover <kaje14@bellsouth.net>
2018-08-20 00:07:50 -0600

    added favicon

--- public/apps/newsScraper/app.js
+++ public/apps/newsScraper/app.js
@@ -76,5 +76,14 @@ $("#button").on("click", function(event) {
   
   });
 
+  $(function () {
+    var nua = navigator.userAgent
+    var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
+    
+    if (isAndroid) {
+        $('select.form-control').removeClass('form-control').css('width', '100%')
+    }
+});
+
  
   
